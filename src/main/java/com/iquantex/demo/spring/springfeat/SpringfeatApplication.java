592673ac5cdc336182bf5bf6f271af894e908455
david.yang <yiwei.yang@iquantex.com>
2020-06-24 01:42:33 +0800

    feat:test Constructor binding

--- src/main/java/com/iquantex/demo/spring/springfeat/SpringfeatApplication.java
+++ src/main/java/com/iquantex/demo/spring/springfeat/SpringfeatApplication.java
@@ -1,11 +1,14 @@
 package com.iquantex.demo.spring.springfeat;
 
+import com.iquantex.demo.spring.springfeat.external.config.AcmeProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@ConfigurationPropertiesScan(basePackageClasses = AcmeProperties.class)
 public class SpringfeatApplication {
 
 	public static void main(String[] args) {
